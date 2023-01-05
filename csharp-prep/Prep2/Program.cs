556502e17fb8437f08caddec002406773f018b97
lizzywooda <104384475+lizzywooda@users.noreply.github.com>
2023-01-05 14:53:29 -0700

    Finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Gets the user input and changes it to a usable int.
+        Console.WriteLine("What is your grade percentage?: ");
+        var input = Console.ReadLine();
+        var percentage = int.Parse(input);
+        // Sets the letter variable
+        char letter = 'F';
+        // Determinds what the letter should be.
+        if (percentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (percentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (percentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (percentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+        // Reports the letter grade.
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (letter == 'F' || letter == 'D')
+        {
+            Console.WriteLine("Sorry you did not pass.");
+        }
+        else
+        {
+            Console.WriteLine("You passed! congrats!");
+        }
     }
 }
