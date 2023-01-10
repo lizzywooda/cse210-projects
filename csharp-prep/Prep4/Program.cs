b04352ed265dd1c762cec10a3999ca932fd72533
lizzywooda <104384475+lizzywooda@users.noreply.github.com>
2023-01-10 14:23:33 -0700

    Finished Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter a number to add to the list, enter 0 to quit: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int total = 0;
+        
+        foreach (int i in numbers)
+        {
+            total += i;
+        }
+
+        Console.WriteLine($"The total is: {total}");
+
+        float avg = total / numbers.Count;
+        Console.WriteLine($"The average is: {avg}");
+
+        int max = numbers[0];
+
+        foreach (int i in numbers)
+        {
+            if (max < i)
+            {
+                max = i;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
