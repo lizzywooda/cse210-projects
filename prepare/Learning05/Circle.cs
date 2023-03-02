f5a4c852be322cd547a64a102ea01d0e5c30f844
lizzywooda <104384475+lizzywooda@users.noreply.github.com>
2023-03-01 23:14:25 -0700

    Finished Learning05

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,14 @@
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string colour, double radius) : base (colour)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+}
