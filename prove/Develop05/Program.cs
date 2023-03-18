fc48b59613c9572bf5f6260093e9ef62651b5458
lizzywooda <104384475+lizzywooda@users.noreply.github.com>
2023-03-18 14:11:13 -0600

    Finished Clean up

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -12,9 +12,9 @@ class Program
             Console.WriteLine("Menu Options:");
             Console.WriteLine("   1. Create New Goal");
             Console.WriteLine("   2. List Goals");
-            Console.WriteLine("   3. Save Goals To File");
-            Console.WriteLine("   4. Load Goals From File");
-            Console.WriteLine("   5. Record A Completed Goal");
+            Console.WriteLine("   3. Save Goals");
+            Console.WriteLine("   4. Load Goals");
+            Console.WriteLine("   5. Record Event");
             Console.WriteLine("   6. Delete A Goal");
             Console.WriteLine("   7. Quit");
             Console.Write("Select a choice from the menu: ");
