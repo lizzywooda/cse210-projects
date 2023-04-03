1fc3e58ce619a55b43ef5148d099bb100301915b
lizzywooda <104384475+lizzywooda@users.noreply.github.com>
2023-04-03 15:16:46 -0600

    Finish Foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,14 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.Clear();
         List<Activity> activity1 = new List<Activity>();
         Running run1 = new Running("03 Nov 2022",30 ,"Running", 3);
         activity1.Add(run1);
 
-        Swimming swim1 = new Swimming("04 Nov 2022", 60, "Swimming",10);
+        Swimming swim1 = new Swimming("03 Nov 2022", 30, "Swimming",5);
         activity1.Add(swim1);
 
-        Bike bike1 = new Bike("05 Nov 2022", 60, "Biking", 12);
+        Bike bike1 = new Bike("07 Nov 2022", 60, "Biking", 8);
         activity1.Add(bike1);
         Console.WriteLine();
         Console.WriteLine("Activity List");
